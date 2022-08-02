65d25208c46b69010048813a3d8d48fc9200dc42
vitormiura <vitormiura3@gmail.com>
2022-08-02 08:52:33 -0300

    Init main file

---
+++ api/main.py
@@ -0,0 +1,28 @@
+import models 
+from handler import SessionLocal, engine
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    title = "BOSCH/ETS Project Manager (Made by ApeView)",
+    version = "0.0.1"
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
